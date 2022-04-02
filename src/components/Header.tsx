210cd69be392eb9303dcbd859553b8ddf1c8cc1a
Mohammed Rabil <rabil@techie.com>
2022-04-02 15:00:28 +0530

    set header font size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header(props: { title: string }) {
         className="animate-spin h-16 w-16"
         alt="logo"
       />
-      <h1 className="text-center text-xl">{props.title}</h1>
+      <h1 className="text-center">{props.title}</h1>
     </div>
   );
 }
