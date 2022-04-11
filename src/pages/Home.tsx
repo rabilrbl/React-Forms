7801098d3cdb716ef4ce889f841fc567d630d558
Mohammed Rabil <rabil@techie.com>
2022-04-11 22:36:37 +0530

    Fix Next Button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -246,7 +246,6 @@ export default function Home() {
               }&search=${search ? search : ""}`;
             }
           }}
-          disabled={forms.length <= 10}
         >
           Next&nbsp;<svg
             xmlns="http://www.w3.org/2000/svg"
