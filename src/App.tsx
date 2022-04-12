c0b86965d7e5856a57d2fd071dd33974a3d9fd92
Mohammed Rabil <rabil@techie.com>
2022-04-12 19:52:29 +0530

    Made App as PWA

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { useRoutes, Redirect } from "raviger";
 import "./App.css";
 import AppContainer from "./components/AppContainer";
-import Form from "./pages/Form";
 import Block from "./components/Block";
-import Home from "./pages/Home";
 import About from "./pages/About";
 import { NavBar } from "./components/NavBar";
 import Preview from "./pages/Preview";
@@ -12,12 +10,22 @@ import { Login } from "./pages/Login";
 import { Logout } from "./pages/Logout";
 import { Signup } from "./pages/Signup";
 import NotFound from "./pages/NotFound";
+import Loading from "./components/Loading";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Form = React.lazy(() => import("./pages/Form"));
 
 const routes = {
-  "/": () => <Home />,
+  "/": () => (
+    <React.Suspense fallback={<Loading />}>
+      <Home />
+    </React.Suspense>
+  ),
   "/about": () => <About />,
   "/form/:id": ({ id }: { id: string }) => (
-    <Form action="" id={Number(id)} method="" />
+    <React.Suspense fallback={<Loading />}>
+      <Form action="" id={Number(id)} method="" />
+    </React.Suspense>
   ),
   "/preview/:formId": ({ formId }: { formId: string }) => (
     <Redirect to={`/preview/${formId}/0`} />
